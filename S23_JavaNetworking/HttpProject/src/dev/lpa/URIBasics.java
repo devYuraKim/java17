764833252c573d85a1d87e8998dbd6aa56c20b70
devYuraKim <dev.yurakim@gmail.com>
2024-10-26 00:50:50 +0900

    URL,URI and HttpUrlConnection

---
+++ S23_JavaNetworking/HttpProject/src/dev/lpa/URIBasics.java
@@ -0,0 +1,68 @@
+package dev.lpa;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class URIBasics {
+
+    public static void main(String[] args) {
+
+        //URI yuraSite = URI.create("https://yurakim.webpage/courses/java");
+        URI baseSite = URI.create("https://yurakim.webpage");
+        URI yuraSite = URI.create("courses/java");
+        print(yuraSite);
+
+        try {
+            URI uri = new URI("http://user:pw@store.com:5000/products/phones?os=android#samsung");
+            print(uri);
+
+            //URL url = uri.toURL();
+            URI masterClass = baseSite.resolve(yuraSite);
+            URL url = masterClass.toURL();
+            System.out.println(url);
+            print(url);
+
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void print(URI uri){
+        System.out.printf("""
+                ----------------------------------------
+                [scheme:]scheme-specific-part[#fragment]
+                -----------------------------------------
+                Scheme: %s
+                Scheme-specific part: %s
+                    Authority: %s
+                        User info: %s
+                        Host: %s
+                        Port: %d
+                        Path: %s
+                        Query : %s          
+                Fragment: %s    
+                """,
+                uri.getScheme(), uri.getSchemeSpecificPart(),
+                uri.getAuthority(), uri.getUserInfo(), uri.getHost(), uri.getPort(),uri.getPath(), uri.getQuery(),
+                uri.getFragment()
+                );
+    }
+
+    private static void print(URL url){
+        System.out.printf("""
+                ----------------------------------------
+                    Authority: %s
+                        User info: %s
+                        Host: %s
+                        Port: %d
+                        Path: %s
+                        Query : %s          
+                """,
+                url.getAuthority(), url.getUserInfo(), url.getHost(), url.getPort(),url.getPath(), url.getQuery()
+        );
+    }
+
+}
