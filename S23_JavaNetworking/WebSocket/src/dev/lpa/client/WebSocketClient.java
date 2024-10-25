9d52e0c8dc1330f485e96b2b610c032adc666649
devYuraKim <dev.yurakim@gmail.com>
2024-10-25 20:50:26 +0900

    complete WebSocket chat app

--- S23_JavaNetworking/WebSocket/src/dev/lpa/client/WebSocketClient.java
+++ S23_JavaNetworking/WebSocket/src/dev/lpa/client/WebSocketClient.java
@@ -4,13 +4,42 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
+import java.util.Scanner;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class WebSocketClient {
 
     public static void main(String[] args) throws URISyntaxException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name to join chat: ");
+        String name = scanner.nextLine();
+
         HttpClient client = HttpClient.newHttpClient();
         WebSocket webSocket = client.newWebSocketBuilder().
-                buildAsync(new URI("ws://localhost:9090"),
-                        new WebSocket.Listener(){}).join();
+                buildAsync(new URI("ws://localhost:9090?name=%s".formatted(name)),
+                        new WebSocket.Listener(){
+                            @Override
+                            public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+                                System.out.println(data);
+                                return WebSocket.Listener.super.onText(webSocket, data, last);
+                            }
+                        }).join();
+
+        while(true){
+            String input = scanner.nextLine();
+            if("bye".equalsIgnoreCase(input)){
+                try {
+                    webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "User Left Normally").get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            }else{
+                webSocket.sendText(input, true);
+            }
+        }
+
     }
 }
--- S23_JavaNetworking/WebSocket/src/dev/lpa/server/SimpleWebSocketServer.java
+++ S23_JavaNetworking/WebSocket/src/dev/lpa/server/SimpleWebSocketServer.java
@@ -5,10 +5,14 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleWebSocketServer extends WebSocketServer {
 
     public static final int SERVER_PORT = 9090;
+    private static Map<String, String> map = new HashMap<>();
 
     public SimpleWebSocketServer() {
         super(new InetSocketAddress(SERVER_PORT));
@@ -21,7 +25,12 @@ public class SimpleWebSocketServer extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        var resource = webSocket.getResourceDescriptor();
+        String name = resource.split("=")[1];
+        map.put(webSocket.getRemoteSocketAddress().toString(),name);
+        System.out.println(map.values());
         System.out.println("Connection opened: " + webSocket.getRemoteSocketAddress());
+        broadcastAllButSender(webSocket, "%s joined".formatted(name));
     }
 
     @Override
@@ -31,9 +40,16 @@ public class SimpleWebSocketServer extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket webSocket, String s) {
-        System.out.println("Message received: " + webSocket.getRemoteSocketAddress());
+        //System.out.println("Message received: " + webSocket.getRemoteSocketAddress());
+        String chatName = map.get(webSocket.getRemoteSocketAddress().toString());
+        broadcastAllButSender(webSocket, "%s: %s".formatted(chatName, s));
     }
 
+    private void broadcastAllButSender(WebSocket webSocket, String message){
+        var connections = new ArrayList<>(getConnections());
+        connections.remove(webSocket);
+        broadcast(message, connections);
+    }
     @Override
     public void onError(WebSocket webSocket, Exception e) {
         System.out.println("Error: " + webSocket.getRemoteSocketAddress());
