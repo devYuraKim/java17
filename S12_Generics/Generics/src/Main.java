1989e8b8bbeed3998ba54ce9ba708704d4848884
devYuraKim <dev.yurakim@gmail.com>
2024-11-01 08:07:49 +0900

    implement upper bound, multiple types

--- S12_Generics/Generics/src/Main.java
+++ S12_Generics/Generics/src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-interface Player{ }
+interface Player{
+    String name();
+}
 
 record BaseballPlayer(String name, String position) implements Player{}
 record FootballPlayer(String name, String position) implements Player{}
@@ -8,6 +10,9 @@ record FootballPlayer(String name, String position) implements Player{}
 
 public class Main {
     public static void main(String[] args) {
+
+        var philly = new Affiliation("city", "PA", "US");
+        var astro = new Affiliation("city", "HT", "US");
 
         BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies1");
         BaseballTeam astros1 = new BaseballTeam("Houston Astros1");
@@ -17,25 +22,37 @@ public class Main {
         SportsTeam astros2 = new SportsTeam("Houston Astros2");
         scoreResult(phillies2, 3, astros2, 5);
 
-        Team<BaseballPlayer> phillies = new Team<>("Philadelphia Phillies");
-        Team<BaseballPlayer> astros = new Team<>("Houston Astros");
+        Team<BaseballPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer, Affiliation> astros = new Team<>("Houston Astros", astro);
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right");
         var marsh = new BaseballPlayer("B Marsh", "Right");
         phillies.addTeamMember(harper);
-        phillies.addTeamMember(marsh);
+        astros.addTeamMember(marsh);
         phillies.listTeamMembers();
+        astros.listTeamMembers();
 
         SportsTeam afc1 = new SportsTeam("Crows1");
-        Team<FootballPlayer> afc = new Team<>("Crows1");
-
+        Team<FootballPlayer, Affiliation> afc = new Team<>("Crows1");
         var tex = new FootballPlayer("Tex", "Mid");
+
         afc.addTeamMember(tex);
         //Type Checking in action
         //afc.addTeamMember(harper);
         afc.listTeamMembers();
 
+        //Problem1: No limit on Type
+        //Solution: Set an upper bound with <T extends Type>
+        //Team<String> seoul = new Team<>("Bears");
+        //seoul.addTeamMember("Seoul player");
+        //seoul.listTeamMembers();
+        //
+        //var busan = new Team<String>("Giants");
+        //busan.addTeamMember("Busan player");
+        //busan.listTeamMembers();
+        //
+        //scoreResult(seoul, 0, busan, 1);
     }
 
     public static void scoreResult(BaseballTeam team1, int t1Score, BaseballTeam team2, int t2Score){
--- S12_Generics/Generics/src/Team.java
+++ S12_Generics/Generics/src/Team.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
+record Affiliation(String name, String type, String countryCode){
+    @Override
+    public String toString() {
+        return name+" (" + type + " in " + countryCode + ")";
+    }
+}
+
 //T stands for Type
 //multiple types are applicable
-public class Team<T> {
+public class Team<T extends Player, S> {
 
     private String teamName;
     private List<T> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
+    private S affiliation;
 
     public Team(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public Team(String teamName, S affiliation) {
+        this.affiliation = affiliation;
         this.teamName = teamName;
     }
 
@@ -22,10 +35,15 @@ public class Team<T> {
         }
     }
 
-    public void listTeamMembers(){
-        System.out.println(teamName+ " Roster:");
+    public void listTeamMembers() {
+        System.out.println(teamName + " Roster:");
         //List has an implicitly defined toString() method
-        System.out.println(teamMembers);
+        //System.out.println(teamMembers);
+        System.out.println(affiliation==null? "" : "===" + affiliation);
+        for(T t: teamMembers){
+            //generic type parameter T with an upper bound set to an interface that contains a name() method
+            System.out.println(t.name());
+        }
     }
 
     public int ranking(){
