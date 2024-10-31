0f7545a07b9e230a63ebe1cd377bbd9786483904
devYuraKim <dev.yurakim@gmail.com>
2024-11-01 06:13:12 +0900

    set up FileListings

---
+++ S20_InputOutput/FileListings/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        Path path = Path.of("");
+        System.out.println("Current Working Directory: " + path.toAbsolutePath());
+
+        Stream<Path> paths =
+
+    }//main method
+
+
+}//Main class
