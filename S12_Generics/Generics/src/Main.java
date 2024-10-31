a6d9ebd661bfae94b4f5c2b0a3f9eef527c19dac
devYuraKim <dev.yurakim@gmail.com>
2024-11-01 06:57:55 +0900

    use Interface
    problem: no type checking

--- S12_Generics/Generics/src/Main.java
+++ S12_Generics/Generics/src/Main.java
@@ -1,12 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-record BaseballPlayer(String name, String position){}
+interface Player{ }
+
+record BaseballPlayer(String name, String position) implements Player{}
+record FootballPlayer(String name, String position) implements Player{}
+
 
 public class Main {
     public static void main(String[] args) {
 
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies1");
+        BaseballTeam astros1 = new BaseballTeam("Houston Astros1");
+        scoreResult(phillies1, 3, astros1, 5);
+
+        SportsTeam phillies = new SportsTeam("Philadelphia Phillies");
+        SportsTeam astros = new SportsTeam("Houston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         var harper = new BaseballPlayer("B Harper", "Right");
@@ -15,9 +23,22 @@ public class Main {
         phillies.addTeamMember(marsh);
         phillies.listTeamMembers();
 
+        SportsTeam afc = new SportsTeam("Crows");
+        var tex = new FootballPlayer("Tex", "Mid");
+        afc.addTeamMember(tex);
+        //Problem1: no type checking
+        afc.addTeamMember(harper);
+        afc.listTeamMembers();
+
     }
 
     public static void scoreResult(BaseballTeam team1, int t1Score, BaseballTeam team2, int t2Score){
+        String message = team1.setScore(t1Score, t2Score);
+        team2.setScore(t2Score, t1Score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(SportsTeam team1, int t1Score, SportsTeam team2, int t2Score){
         String message = team1.setScore(t1Score, t2Score);
         team2.setScore(t2Score, t1Score);
         System.out.printf("%s %s %s %n", team1, message, team2);
